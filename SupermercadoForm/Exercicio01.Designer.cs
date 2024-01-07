f7e4b44681543210edeb90bfc4473775cbd536fd
Enzo Giacomelli <egiacomelli09@gmail.com>
2024-01-07 18:57:20 -0300

    exercicio 01

---
+++ SupermercadoForm/Exercicio01.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SupermercadoForm
+{
+    partial class Exercicio01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelIdade = new System.Windows.Forms.Label();
+            this.labelNota1 = new System.Windows.Forms.Label();
+            this.labelNota2 = new System.Windows.Forms.Label();
+            this.labelNota3 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.numericUpDownIdade = new System.Windows.Forms.NumericUpDown();
+            this.textBoxNota1 = new System.Windows.Forms.TextBox();
+            this.textBoxNota2 = new System.Windows.Forms.TextBox();
+            this.textBoxNota3 = new System.Windows.Forms.TextBox();
+            this.labelMedia = new System.Windows.Forms.Label();
+            this.buttonCalcularMedia = new System.Windows.Forms.Button();
+            this.labelSituacao = new System.Windows.Forms.Label();
+            this.textBoxMedia = new System.Windows.Forms.TextBox();
+            this.textSituacao = new System.Windows.Forms.TextBox();
+            this.groupBoxAluno = new System.Windows.Forms.GroupBox();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIdade)).BeginInit();
+            this.groupBoxAluno.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(6, 19);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 0;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelIdade
+            // 
+            this.labelIdade.AutoSize = true;
+            this.labelIdade.Location = new System.Drawing.Point(8, 63);
+            this.labelIdade.Name = "labelIdade";
+            this.labelIdade.Size = new System.Drawing.Size(36, 15);
+            this.labelIdade.TabIndex = 1;
+            this.labelIdade.Text = "Idade";
+            // 
+            // labelNota1
+            // 
+            this.labelNota1.AutoSize = true;
+            this.labelNota1.Location = new System.Drawing.Point(86, 63);
+            this.labelNota1.Name = "labelNota1";
+            this.labelNota1.Size = new System.Drawing.Size(42, 15);
+            this.labelNota1.TabIndex = 2;
+            this.labelNota1.Text = "Nota 1";
+            // 
+            // labelNota2
+            // 
+            this.labelNota2.AutoSize = true;
+            this.labelNota2.Location = new System.Drawing.Point(8, 107);
+            this.labelNota2.Name = "labelNota2";
+            this.labelNota2.Size = new System.Drawing.Size(42, 15);
+            this.labelNota2.TabIndex = 3;
+            this.labelNota2.Text = "Nota 2";
+            // 
+            // labelNota3
+            // 
+            this.labelNota3.AutoSize = true;
+            this.labelNota3.Location = new System.Drawing.Point(86, 107);
+            this.labelNota3.Name = "labelNota3";
+            this.labelNota3.Size = new System.Drawing.Size(42, 15);
+            this.labelNota3.TabIndex = 4;
+            this.labelNota3.Text = "Nota 3";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(8, 37);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(152, 23);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // numericUpDownIdade
+            // 
+            this.numericUpDownIdade.Location = new System.Drawing.Point(8, 81);
+            this.numericUpDownIdade.Name = "numericUpDownIdade";
+            this.numericUpDownIdade.Size = new System.Drawing.Size(72, 23);
+            this.numericUpDownIdade.TabIndex = 2;
+            // 
+            // textBoxNota1
+            // 
+            this.textBoxNota1.Location = new System.Drawing.Point(86, 81);
+            this.textBoxNota1.Name = "textBoxNota1";
+            this.textBoxNota1.Size = new System.Drawing.Size(74, 23);
+            this.textBoxNota1.TabIndex = 3;
+            // 
+            // textBoxNota2
+            // 
+            this.textBoxNota2.Location = new System.Drawing.Point(6, 125);
+            this.textBoxNota2.Name = "textBoxNota2";
+            this.textBoxNota2.Size = new System.Drawing.Size(74, 23);
+            this.textBoxNota2.TabIndex = 4;
+            // 
+            // textBoxNota3
+            // 
+            this.textBoxNota3.Location = new System.Drawing.Point(86, 125);
+            this.textBoxNota3.Name = "textBoxNota3";
+            this.textBoxNota3.Size = new System.Drawing.Size(74, 23);
+            this.textBoxNota3.TabIndex = 5;
+            // 
+            // labelMedia
+            // 
+            this.labelMedia.AutoSize = true;
+            this.labelMedia.Location = new System.Drawing.Point(10, 180);
+            this.labelMedia.Name = "labelMedia";
+            this.labelMedia.Size = new System.Drawing.Size(40, 15);
+            this.labelMedia.TabIndex = 10;
+            this.labelMedia.Text = "Média";
+            // 
+            // buttonCalcularMedia
+            // 
+            this.buttonCalcularMedia.Location = new System.Drawing.Point(6, 154);
+            this.buttonCalcularMedia.Name = "buttonCalcularMedia";
+            this.buttonCalcularMedia.Size = new System.Drawing.Size(154, 23);
+            this.buttonCalcularMedia.TabIndex = 6;
+            this.buttonCalcularMedia.Text = "Calcular Média";
+            this.buttonCalcularMedia.UseVisualStyleBackColor = true;
+            this.buttonCalcularMedia.Click += new System.EventHandler(this.buttonCalcularMedia_Click);
+            // 
+            // labelSituacao
+            // 
+            this.labelSituacao.AutoSize = true;
+            this.labelSituacao.Location = new System.Drawing.Point(86, 180);
+            this.labelSituacao.Name = "labelSituacao";
+            this.labelSituacao.Size = new System.Drawing.Size(52, 15);
+            this.labelSituacao.TabIndex = 12;
+            this.labelSituacao.Text = "Situação";
+            // 
+            // textBoxMedia
+            // 
+            this.textBoxMedia.Location = new System.Drawing.Point(6, 198);
+            this.textBoxMedia.Name = "textBoxMedia";
+            this.textBoxMedia.ReadOnly = true;
+            this.textBoxMedia.Size = new System.Drawing.Size(74, 23);
+            this.textBoxMedia.TabIndex = 7;
+            // 
+            // textSituacao
+            // 
+            this.textSituacao.Location = new System.Drawing.Point(86, 198);
+            this.textSituacao.Name = "textSituacao";
+            this.textSituacao.ReadOnly = true;
+            this.textSituacao.Size = new System.Drawing.Size(74, 23);
+            this.textSituacao.TabIndex = 8;
+            // 
+            // groupBoxAluno
+            // 
+            this.groupBoxAluno.Controls.Add(this.buttonLimpar);
+            this.groupBoxAluno.Controls.Add(this.labelNome);
+            this.groupBoxAluno.Controls.Add(this.textSituacao);
+            this.groupBoxAluno.Controls.Add(this.labelIdade);
+            this.groupBoxAluno.Controls.Add(this.textBoxMedia);
+            this.groupBoxAluno.Controls.Add(this.labelNota1);
+            this.groupBoxAluno.Controls.Add(this.labelSituacao);
+            this.groupBoxAluno.Controls.Add(this.labelNota2);
+            this.groupBoxAluno.Controls.Add(this.buttonCalcularMedia);
+            this.groupBoxAluno.Controls.Add(this.labelNota3);
+            this.groupBoxAluno.Controls.Add(this.labelMedia);
+            this.groupBoxAluno.Controls.Add(this.textBoxNome);
+            this.groupBoxAluno.Controls.Add(this.textBoxNota3);
+            this.groupBoxAluno.Controls.Add(this.numericUpDownIdade);
+            this.groupBoxAluno.Controls.Add(this.textBoxNota2);
+            this.groupBoxAluno.Controls.Add(this.textBoxNota1);
+            this.groupBoxAluno.Location = new System.Drawing.Point(12, 11);
+            this.groupBoxAluno.Name = "groupBoxAluno";
+            this.groupBoxAluno.Size = new System.Drawing.Size(171, 267);
+            this.groupBoxAluno.TabIndex = 15;
+            this.groupBoxAluno.TabStop = false;
+            this.groupBoxAluno.Text = "Aluno";
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Location = new System.Drawing.Point(6, 227);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(155, 23);
+            this.buttonLimpar.TabIndex = 9;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // Exercicio01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 450);
+            this.Controls.Add(this.groupBoxAluno);
+            this.Name = "Exercicio01";
+            this.Text = "Exercicio01";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIdade)).EndInit();
+            this.groupBoxAluno.ResumeLayout(false);
+            this.groupBoxAluno.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label labelNome;
+        private Label labelIdade;
+        private Label labelNota1;
+        private Label labelNota2;
+        private Label labelNota3;
+        private TextBox textBoxNome;
+        private NumericUpDown numericUpDownIdade;
+        private TextBox textBoxNota1;
+        private TextBox textBoxNota2;
+        private TextBox textBoxNota3;
+        private Label labelMedia;
+        private Button buttonCalcularMedia;
+        private Label labelSituacao;
+        private TextBox textBoxMedia;
+        private TextBox textSituacao;
+        private GroupBox groupBoxAluno;
+        private Button buttonLimpar;
+    }
+}
---
+++ SupermercadoForm/Exercicio01.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupermercadoForm
+{
+    public partial class Exercicio01 : Form
+    {
+        public Exercicio01()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalcularMedia_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3;
+
+            try
+            {
+                nota1 = Convert.ToDouble(textBoxNota1.Text);
+                if(nota1 < 0 || nota1 >10)
+                {
+                    MessageBox.Show("A nota 1 deve ser um valor numérico de 0 a 10.");
+                    textBoxNota1.Focus();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A nota 1 deve ser um valor numérico de 0 a 10.");
+                textBoxNota1.Focus();
+                return;
+            }
+
+            try
+            {
+                nota2 = Convert.ToDouble(textBoxNota2.Text);
+                if (nota2 < 0 || nota2 > 10)
+                {
+                    MessageBox.Show("A nota 2 deve ser um valor numérico de 0 a 10.");
+                    textBoxNota2.Focus();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A nota 2 deve ser um valor numérico de 0 a 10.");
+                textBoxNota2.Focus();
+                return;
+            }
+
+            try
+            {
+                nota3 = Convert.ToDouble(textBoxNota3.Text);
+                if (nota3 < 0 || nota3 > 10)
+                {
+                    MessageBox.Show("A nota 3 deve ser um valor numérico de 0 a 10.");
+                    textBoxNota3.Focus();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A nota 3 deve ser um valor numérico de 0 a 10.");
+                textBoxNota3.Focus();
+                return;
+            }
+
+            double media = (nota1 + nota2 + nota3) / 3.0;
+            string situacao = "";
+
+            if(media < 6.75)
+            {
+                situacao = "Reprovado";
+            }
+            else if (media >= 6.75)
+            {
+                situacao = "Aprovado";
+            }
+
+            textBoxMedia.Text = media.ToString();
+            textSituacao.Text = situacao;
+
+            Limpar();
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            Limpar();
+            textBoxMedia.Clear();
+            textSituacao.Clear();
+        }
+
+        public void Limpar()
+        {
+            textBoxNota1.Clear();
+            textBoxNota2.Clear();
+            textBoxNota3.Clear();
+        }
+    }
+}
